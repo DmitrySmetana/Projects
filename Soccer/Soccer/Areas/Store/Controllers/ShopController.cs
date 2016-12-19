458bf1ae3c1874e55c64ebe5dcc768f2a92cbdda
Dmitry Smetana <dmitry.smetana@gmail.com>
2016-12-19 17:25:44 +0200

    Shop Controller Commit

---
+++ Soccer/Soccer/Areas/Store/Controllers/ShopController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Soccer.Models;
+using System.Data.Entity;
+
+namespace Soccer.Areas.Store.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "ShopController";
+        }
+    }
+}
